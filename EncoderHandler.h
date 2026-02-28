94d8aeffa67941dced71a990158c2b90788e5d61
momotsanya <159866546+momotsanya@users.noreply.github.com>
2026-02-28 01:25:15 -0500

    Update EncoderHandler.h

--- EncoderHandler.h
+++ EncoderHandler.h
@@ -8,22 +8,30 @@ public:
     void begin();
     void tick();
 
-    int getDirection();       // +1 = CW, -1 = CCW, 0 = no rotation
-    bool isShortPressed();    // returns true once per short press
-    bool isLongPressed();     // returns true once per long press
+    int getDirection();       // +1 = CW, -1 = CCW, 0 = no movement
+    bool isShortPressed();
+    bool isLongPressed();
 
 private:
     int _pinA, _pinB, _pinButton;
-    int _lastStateA;
 
     enum ButtonState {IDLE, PRESSING, LONG_PRESS};
     ButtonState _buttonState;
 
     unsigned long _buttonPressTime;
-    const unsigned long _longPressTime = 1000; // 1 second
+    const unsigned long _longPressTime = 1000;
 
     bool _shortPressDetected;
     bool _longPressDetected;
 
     int _rotation;
+
+    uint8_t _lastEncoded;
+    int _encoderStepCounter;   // counts partial transitions
+
+    // lookup table for quadrature
+    int8_t _encoderTable[16] = {0, -1, 1, 0,
+                                1, 0, 0, -1,
+                               -1, 0, 0, 1,
+                                0, 1, -1, 0};
 };
